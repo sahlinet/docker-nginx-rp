ff4c45400510ce6d0a0dd7139dbfebb0a9b5fbf6
Philip Sahli <philip@sahli.net>
2016-05-30 06:34:05 -0400

    loop for creating multiple certificates

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
-#export VHOST_1_SSL=True
+#!/bin/bash
 export VHOST_1_SERVERNAME=a.sahli.net
 export VHOST_1_SSL=True
 export VHOST_1_PROXYCONF_home_proxy_pass=http://aaaa
 export VHOST_1_PROXYCONF_home_location=/aaa
 
-#export VHOST_2_SSL=True
-export VHOST_2_SSL=False
 export VHOST_2_SERVERNAME=b.sahli.net
+export VHOST_2_SSL=False
 export VHOST_2_PROXYCONF_home_proxy_pass=http://bbb
 export VHOST_2_PROXYCONF_home_location=/bbb
 export VHOST_2_PROXYCONF_static_proxy_pass=http://b2b2b2
@@ -14,9 +13,38 @@ export VHOST_2_PROXYCONF_static_location=/b2b2b2
 export VHOST_2_PROXYCONF_admin_return=404
 export VHOST_2_PROXYCONF_admin_location=/b2b2b2
 
-python env2json.py | python -m json.tool
+#python env2json.py | python -m json.tool
 python env2json.py > env.json
 j2 nginx.tmpl env.json > nginx.conf
 echo "* Configuration start"
 cat nginx.conf
 echo "* Configuration done"
+
+cnt=1
+echo "****"
+check_var=VHOST_${cnt}_SERVERNAME
+while [ ! x"${!check_var}" == "x" ]; do
+    ssl=VHOST_${cnt}_SSL
+    if [ "${!ssl}" == "True" ]; then
+         echo "* Process SSL for ${!check_var}"
+    fi
+
+    let cnt=cnt+1
+    check_var=VHOST_${cnt}_SERVERNAME
+done
+docker run nginx- rp
+export VHOST_1_SERVERNAME=a.sahli.net
+export VHOST_1_SSL=True
+export VHOST_1_PROXYCONF_home_proxy_pass=http://aaaa
+export VHOST_1_PROXYCONF_home_location=/aaa
+
+export VHOST_2_SERVERNAME=b.sahli.net
+export VHOST_2_SSL=False
+export VHOST_2_PROXYCONF_home_proxy_pass=http://bbb
+export VHOST_2_PROXYCONF_home_location=/bbb
+export VHOST_2_PROXYCONF_static_proxy_pass=http://b2b2b2
+export VHOST_2_PROXYCONF_static_location=/b2b2b2
+export VHOST_2_PROXYCONF_admin_return=404
+export VHOST_2_PROXYCONF_admin_location=/b2b2b2
+
+
