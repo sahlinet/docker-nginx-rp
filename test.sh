7350c8e4336aaa600b39c0fb25b031dd959eff35
Philip Sahli <philip@sahli.net>
2016-09-11 07:51:18 -0400

    fix test

--- test.sh
+++ test.sh
@@ -4,14 +4,14 @@ export VHOST_1_SSL=True
 export VHOST_1_PROXYCONF_home_proxy_pass=http://aaaa
 export VHOST_1_PROXYCONF_home_location=/aaa
 
-export VHOST_2_SERVERNAME=b.sahli.net
-export VHOST_2_SSL=False
-export VHOST_2_PROXYCONF_home_proxy_pass=http://bbb
-export VHOST_2_PROXYCONF_home_location=/bbb
-export VHOST_2_PROXYCONF_static_proxy_pass=http://b2b2b2
-export VHOST_2_PROXYCONF_static_location=/b2b2b2
-export VHOST_2_PROXYCONF_admin_return=404
-export VHOST_2_PROXYCONF_admin_location=/b2b2b2
+#export VHOST_2_SERVERNAME=b.sahli.net
+#export VHOST_2_SSL=False
+#export VHOST_2_PROXYCONF_home_proxy_pass=http://bbb
+#export VHOST_2_PROXYCONF_home_location=/bbb
+#export VHOST_2_PROXYCONF_static_proxy_pass=http://b2b2b2
+#export VHOST_2_PROXYCONF_static_location=/b2b2b2
+#export VHOST_2_PROXYCONF_admin_return=404
+#export VHOST_2_PROXYCONF_admin_location=/b2b2b2
 
 #python env2json.py | python -m json.tool
 python env2json.py > env.json
@@ -19,6 +19,8 @@ j2 nginx.tmpl env.json > nginx.conf
 echo "* Configuration start"
 cat nginx.conf
 echo "* Configuration done"
+
+exit 1
 
 cnt=1
 echo "****"
@@ -32,7 +34,7 @@ while [ ! x"${!check_var}" == "x" ]; do
     let cnt=cnt+1
     check_var=VHOST_${cnt}_SERVERNAME
 done
-docker run nginx- rp
+docker run nginx-rp
 export VHOST_1_SERVERNAME=a.sahli.net
 export VHOST_1_SSL=True
 export VHOST_1_PROXYCONF_home_proxy_pass=http://aaaa
