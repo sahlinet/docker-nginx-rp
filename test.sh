3015464694c725c4353eb5cfa89dbe27b477ff7b
Philip Sahli <philip@sahli.net>
2016-09-23 02:14:47 -0400

    add no_proxy_header option, add COOKIEPATH/COOKIEREPLACEMENT option

--- test.sh
+++ test.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+unset VHOST_2_DOMAINS
+unset VHOST_2_SERVERNAME
+unset VHOST_2_SSL
+unset VHOST_2_PROXYCONF_all_proxy_pass
+unset VHOST_2_PROXYCONF_notify_proxy_pass
+
 export VHOST_1_SERVERNAME=a.sahli.net
 export VHOST_1_SSL=True
+export VHOST_1_COOKIEPATH=/userland/admin/httptest
+export VHOST_1_COOKIEREPLACEMENT=/
 export VHOST_1_PROXYCONF_home_proxy_pass=http://aaaa
 export VHOST_1_PROXYCONF_home_location=/aaa
 export VHOST_1_PROXYCONF_plain_proxy_pass=http://aaaa/plain
 export VHOST_1_PROXYCONF_plain_plain=True
 export VHOST_1_PROXYCONF_plain_location=/plain
+export VHOST_1_PROXYCONF_host_proxy_pass=http://aaaa
+export VHOST_1_PROXYCONF_host_no_proxy_header=True
+export VHOST_1_PROXYCONF_host_location=/host
 
 #export VHOST_2_SERVERNAME=b.sahli.net
 #export VHOST_2_SSL=False
